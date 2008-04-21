2ac40f75b40bfbea80d5b809eabc2fe1047a8871
jbarnier (Julien Barnier) <julien.barnier@ens-lyon.fr>
2008-04-21 13:11:51 +0000

    Ajout de la fonction rgrs.update()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgrs@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/rgrs.update.R
@@ -0,0 +1,12 @@
+`rgrs.update` <-
+function() {
+  installed.rgrs.version <- installed.packages()["rgrs","Version"]
+  cat("Version de rgrs installee sur le systeme :", installed.rgrs.version, "\n")
+  remote.rgrs.version <- available.packages(contrib.url("http://r-forge.r-project.org/"))["rgrs","Version"]
+  cat("Version de rgrs disponible sur r-forge :", remote.rgrs.version, "\n")
+  if (installed.rgrs.version == remote.rgrs.version) cat("Pas de nouvelle version a installer\n")
+  else {
+    install.packages("rgrs",repos="http://r-forge.r-project.org")
+    cat("Mise a jour effectuee\n")
+  }
+}
