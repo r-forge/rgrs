18ac0628f8149c2093ebdef5187fb6c7e81efebe
jbarnier (Julien Barnier) <julien.barnier@ens-lyon.fr>
2008-07-17 14:53:43 +0000

    Ajout de l'export dans un fichier à la fonction copie
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgrs@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/copie.R
+++ pkg/R/copie.R
@@ -1,5 +1,9 @@
 `copie` <-
-function (obj, append=FALSE, ...) {
+function (obj, append=FALSE, file=FALSE, filename="temp.html", ...) {
+  if (file) {
+    HTML(obj, file=filename, append=append)
+    return
+  }
   if (Sys.info()["sysname"] == "Windows") conn <- file("clipboard", "w")
   if (Sys.info()["sysname"] == "Darwin") conn <- pipe("pbcopy", "w")
   if (Sys.info()["sysname"] == "Linux") conn <- pipe("xclip -i", "w")
