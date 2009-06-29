ab564f00fe101268440f2d5b25abd339c825184a
jbarnier (Julien Barnier) <julien.barnier@ens-lyon.fr>
2009-06-29 13:39:19 +0000

    Forcage de l'encodage en Latin1 pour les exports R2HTML
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgrs@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/copie.default.R
+++ pkg/R/copie.default.R
@@ -2,12 +2,14 @@
 function (obj, append=FALSE, file=FALSE, filename="temp.html", ...) {
   require(R2HTML)
   if (file) {
-    HTML(obj, file=filename, append=append)
+    conn <- file(filename, "w", encoding="Latin1")
+    HTML(obj, file=conn, append=append)
+    close(conn)
     return
   }
-  if (Sys.info()["sysname"] == "Windows") conn <- file("clipboard", "w")
-  if (Sys.info()["sysname"] == "Darwin") conn <- pipe("pbcopy", "w")
-  if (Sys.info()["sysname"] == "Linux") conn <- pipe("xclip -i", "w")
+  if (Sys.info()["sysname"] == "Windows") conn <- file("clipboard", "w", encoding="Latin1")
+  if (Sys.info()["sysname"] == "Darwin") conn <- pipe("pbcopy", "w", encoding="Latin1")
+  if (Sys.info()["sysname"] == "Linux") conn <- pipe("xclip -i", "w", encoding="Latin1")
   R2HTML::HTML(obj, file = conn, append = append, ...)
   close(conn)
 }
