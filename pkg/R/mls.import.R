7ff89d6e3574bd356a211145a0f749140237dabf
jbarnier (Julien Barnier) <julien.barnier@ens-lyon.fr>
2011-09-26 09:02:53 +0000

    Add rgrs-Ex.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgrs@56 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mls.import.R
+++ pkg/R/mls.import.R
@@ -1,6 +1,6 @@
 `mls.import` <-
 function (filename, enc="latin1", modif.names=TRUE) {
-  tmp <- read.table(file(filename,encoding=enc),header=TRUE,sep="\t",quote="",na.string="")
+  tmp <- read.table(file(filename,encoding=enc),header=TRUE,sep="\t",quote="",na.strings="")
   if (modif.names) {
     colnames(tmp) <- tolower(colnames(tmp))
     colnames(tmp) <- gsub(" ","_",colnames(tmp))
